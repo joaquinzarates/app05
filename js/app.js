cec30bbb79f8cbce4c8e3f2a9a53d9cea20cd43f
Joaquin Zarate Sanchez <joaquin.zarate@ids.com.mx>
2026-04-22 18:00:47 -0600

    feat: anexo de estilos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-const themeBtn = document.getElementById('theme-toggle')
+
+
+//const themeBtn = document.getElementById('theme-toggle')
+const themeToggle = document.getElementById('theme-toggle')
 const htmlelement = document.documentElement;
 
 const applyTheme = (theme) => {
@@ -8,16 +11,22 @@ const applyTheme = (theme) => {
     
 }
 
-const savedTheme = localStorage.getItem('iacon-theme');
+/*const savedTheme = localStorage.getItem('iacon-theme');
 if(savedTheme){
     applyTheme(savedTheme);
 }else{
     applyTheme('dark');
 }
+*/
 
-themeToggle.addEventListener('click',() => {
+if (themeToggle)
+    
+    {themeToggle.addEventListener('click',() => {
 
     const currentTheme = htmlelement.getAttribute('data-theme');
     const nextTheme = currentTheme === 'dark' ? 'light' : 'dark';
     applyTheme(nextTheme);
-})
+    console.log("todo en orden");
+})}
+
+
